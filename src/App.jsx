51613f3c9f6019cfdfa8417426e77c2ae60f90bb
kiranthilak <kiran.thilak@yahoo.com>
2024-10-25 13:10:21 +0530

    blur background animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import BlurBackground from "./components/BlurBackground";
 
 const App = () => {
   return (
     <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_50%_50%_at_50%_-20%,rgba(100,0,100,0.6),rgba(0,0,0,0.8))]">
       <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <BlurBackground />
       </div>
     </div>
   );
