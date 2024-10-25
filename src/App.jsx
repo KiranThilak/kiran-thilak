1c61e91d7b493835e0e0a54a352f3763841c98f6
kiranthilak <kiran.thilak@yahoo.com>
2024-10-25 11:46:10 +0530

    background gradient added

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_50%_50%_at_50%_-20%,rgba(100,0,100,0.6),rgba(0,0,0,0.8))]">
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
     </div>
   );
 };
